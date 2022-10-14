38508df0ae68b7445ad02d96d33a7a446b878fa1
leylshafi <llshafi03@gmail.com>
2022-10-14 18:16:54 +0400

    LAST

--- WinFormsApp1/Repositories/IRepository.cs
+++ WinFormsApp1/Repositories/IRepository.cs
@@ -2,8 +2,8 @@
 
 public interface IRepository<T>
 {
-    T Get(Func<T, bool>predicate);
-    List<T> GetList(Func<T, bool>? predicate=null);
+    T? Get(Func<T, bool>predicate);
+    List<T>? GetList(Func<T, bool>? predicate=null);
 
     void Add(T entity);
     void Update(T entity);
