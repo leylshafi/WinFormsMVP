878928d49717fa9e66818c8375f8df4a9d28554f
leylshafi <llshafi03@gmail.com>
2022-10-14 18:11:37 +0400

    Last

---
+++ WinFormsApp1/Repositories/Contexts/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WinFormsApp1.Models;
+
+namespace WinFormsApp1.Repositories.Contexts;
+
+public  class MyDBContext:DbContext
+{
+    // Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = 'DemoMVP'; Integrated Security = True; 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = DemoMVP; Integrated Security = True; ");
+
+        base.OnConfiguring(optionsBuilder);
+    }
+    public DbSet<Student>? Students { get; set; }
+}
